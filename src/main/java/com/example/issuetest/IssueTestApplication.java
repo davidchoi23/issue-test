829fcd87ec0e53ef7b400932a85b2744d29dd197
David <david.choi@wirebarley.com>
2020-10-16 10:15:25 +0900

    Initial commit

---
+++ src/main/java/com/example/issuetest/IssueTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IssueTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/issuetest/IssueTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issuetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
