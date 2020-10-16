35e80ad5b8ab6a88bdec8f29f565860419b050d3
David <david.choi@wirebarley.com>
2020-10-16 12:52:27 +0900

    이슈 controller 에 postIssue 생성

--- src/main/java/com/example/issuetest/controller/IssueController.java
+++ src/main/java/com/example/issuetest/controller/IssueController.java
@@ -1,6 +1,7 @@
 package com.example.issuetest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,8 @@ public class IssueController {
     return "result issue";
   }
 
-
+  @PostMapping
+  public String postIssue() {
+    return "result post issue";
+  }
 }
